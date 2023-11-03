59eb515d9e4c9edecf1edab02de731bd096b638b
theyarestories <theyarestories@gmail.com>
2023-11-03 20:20:46 +0200

    Small fix

--- src/interfaces/express/Express.ts
+++ src/interfaces/express/Express.ts
@@ -1,12 +1,7 @@
 // declaration merging: https://blog.logrocket.com/extend-express-request-object-typescript/
 
-import { IUser } from "../user/IUser";
-
 declare global {
   namespace Express {
-    export interface Request {
-      user: IUser | null;
-    }
     export interface Response {
       advancedResults: {
         success: boolean;
