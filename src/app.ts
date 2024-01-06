06045c6ec4c636eb2241ace8235241a43ac24936
theyarestories <theyarestories@gmail.com>
2024-01-06 22:25:57 +0200

    Allowed all domains with theyarestories to call the server

--- src/app.ts
+++ src/app.ts
@@ -57,6 +57,7 @@ const corsOptions: CorsOptions = {
   origin: [
     process.env.FRONTEND_URL,
     "http://127.0.0.1",
+    /.*theyarestories.*/,
     // your origins here
   ],
   credentials: true,
