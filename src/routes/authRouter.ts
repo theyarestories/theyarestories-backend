65eea7fa2d9d0134406017a64ae2d84af5041147
theyarestories <theyarestories@gmail.com>
2024-02-09 00:47:25 +0200

    Small fix

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -94,7 +94,9 @@ export default class AuthRouter {
       // Update viewers
       await StoryModel.updateMany(
         { viewers: mixpanelId },
-        { $pull: { viewers: mixpanelId }, $push: { viewers: userId } }
+        // { $pull: { viewers: mixpanelId }, $push: { viewers: userId } },
+        { $set: { "viewers.$[filter]": userId } },
+        { arrayFilters: [{ filter: mixpanelId }] }
       );
     } catch (error) {
       next(error);
